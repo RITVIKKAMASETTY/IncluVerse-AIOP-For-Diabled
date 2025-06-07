bcc6836c5f03c50a11841deec10f2bdf91478b43
Khushbu-github <khushbuhajari530@gmail.com>
2025-06-07 23:13:31 +0530

    Remove .env file and add to .gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+<<<<<<< HEAD
 import App from './App.jsx'
+=======
+import App from './App'
+>>>>>>> f04caa28cdc5691db7a75c8e069e3fe46d66b8dc
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
